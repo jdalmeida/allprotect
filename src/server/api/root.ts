6af13866f5dfe51b150ff2ce8fc4e9b10be75e23
jdalmeida <joaogabrielde.almeida@hotmail.com>
2024-07-07 12:23:49 -0300

    me ajuda deus

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,17 +1,11 @@
-import { postRouter } from "@/server/api/routers/post";
-import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  post: postRouter,
-});
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
 
 /**
  * Create a server-side caller for the tRPC API.
@@ -20,4 +14,3 @@ export type AppRouter = typeof appRouter;
  * const res = await trpc.post.all();
  *       ^? Post[]
  */
-export const createCaller = createCallerFactory(appRouter);
